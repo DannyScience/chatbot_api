0f81576b4502610f284886d869ee2c326d39d899
unknown <s.denis1125@gmail.com>
2023-11-23 23:20:24 -0800

    added async database connector

--- src/config.py
+++ src/config.py
@@ -5,3 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GPT_KEY = os.environ.get("GPT_KEY")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
